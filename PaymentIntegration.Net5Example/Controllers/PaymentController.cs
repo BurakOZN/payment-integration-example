96be778ef6cbb3f7f207425dfda2105e48c0f91e
BurakOZN <48183286+BurakOZN@users.noreply.github.com>
2020-12-30 13:33:48 +0300

    -Ödeme için firmadan gelecek bilgiler tanımlandı. -Ödeme sayfasından gelen bilgiler payment.Auth3D methodu ile ödeme sistemine gönderildi. Gelen response da hata var ise error ile ekrana basıldı. Hata yok ise sayfa gelen response içerisindeki htmlContent e yönlendirildi. -ACS sonucu token verisinin ulaşacağı returnUrl methodu geliştirildi. -Check methodu ile ödeme sonuçu kontrol edildi.

---
+++ PaymentIntegration.Net5Example/Controllers/PaymentController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PaymentIntegration.Manager;
+using PaymentIntegration.Model;
+using PaymentIntegration.Net5Example.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentIntegration.Net5Example.Controllers
+{
+    public class PaymentController : Controller
+    {
+        //Ask for this information
+        private const string ApiUrl = "";
+        private const string ApiKey = "";
+        private const string SecretKey = "";
+
+        PaymentOptions paymentOptions;
+        public PaymentController()
+        {
+            paymentOptions = new PaymentOptions(ApiUrl, ApiKey, SecretKey);
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new ErrorViewModel() { ErrorMessage=""});
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(PaymentModel paymentModel)
+        {
+
+            var auth3DRequest = new Auth3DRequest();
+            auth3DRequest.Amount = 500;//5.00
+            auth3DRequest.CardNo = paymentModel.Cardnumber;
+            auth3DRequest.Currency = 978;
+            auth3DRequest.Cvv2 = paymentModel.Cvv;
+            auth3DRequest.Ecommerce = true;
+            auth3DRequest.Expiry = int.Parse(paymentModel.Expyear.Substring(2, 2) + paymentModel.Expmonth);
+            auth3DRequest.InstallmentCount = 1;
+            auth3DRequest.Lang = "TR";
+            auth3DRequest.OrderId = DateTime.Now.Ticks.ToString();
+
+            var payment = new Payment(paymentOptions);
+            var response = await payment.Auth3D(auth3DRequest);
+            if (response.IsConnectionSuccess)
+                if (response.Result.State == PaymentState.Success)
+                {
+                    var html64 = response.Result.Result.HtmlContent;
+                    var htmlContent = Encoding.UTF8.GetString(Convert.FromBase64String(html64));
+                    return Content(htmlContent, "text/html");
+                }
+                else
+                    return View(new ErrorViewModel() { ErrorMessage = response.Result.Result.ResultMessage });
+            return View(new ErrorViewModel() { ErrorMessage = "Connection Error:" + response.StatusCode });
+        }
+        [HttpGet]
+        public async Task<IActionResult> ReturnUrl(string token)//Your return url must be defined.
+        {
+            var checkPaymentRequest = new CheckPaymentRequest();
+            checkPaymentRequest.Token = token;
+            var payment = new Payment(paymentOptions);
+            var response = await payment.CheckPayment(checkPaymentRequest);
+            if (response.IsConnectionSuccess)
+                return Ok(response.Result.Result);//Just a quick example :)
+            return View();
+        }
+    }
+}
