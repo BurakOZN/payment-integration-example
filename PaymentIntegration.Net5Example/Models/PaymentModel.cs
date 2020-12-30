1c42ac0cb6853427a18eef188f9c76e9a50ae217
BurakOZN <48183286+BurakOZN@users.noreply.github.com>
2020-12-30 13:30:51 +0300

    Ui ve ui model oluşturuldu.

---
+++ PaymentIntegration.Net5Example/Models/PaymentModel.cs
@@ -0,0 +1,10 @@
+﻿namespace PaymentIntegration.Net5Example.Models
+{
+    public class PaymentModel
+    {
+        public string Cardnumber { get; set; }
+        public string Expmonth { get; set; }
+        public string Expyear { get; set; }
+        public string Cvv { get; set; }
+    }
+}
